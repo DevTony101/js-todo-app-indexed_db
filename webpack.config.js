e2e2c643950a2c084a6f7c5cdaca861fef9d92bf
DevTony101 <tonycortesano@gmail.com>
2020-07-13 16:42:08 -0500

    Initial webpack configuration with webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ],
+  },
+};
