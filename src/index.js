39779fce864b923b3ce0facba8747f75a5ca72a7
DevTony101 <tonycortesano@gmail.com>
2020-07-14 18:27:23 -0500

    Polishing starter code. Created saveTask function

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", saveTask);
+
+  function saveTask(event) {
+    event.preventDefault();
+    const title = document.querySelector("#itTitle").value;
+    const description = document.querySelector("#itDescription").value;
+    const task = {title, description};
+    // TODO: Implement the logic for creating new tasks...
+  }
+});
