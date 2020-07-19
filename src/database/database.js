397063909905ea3b6aa9a965d5765b36f9245585
DevTony101 <tonycortesano@gmail.com>
2020-07-19 00:32:54 -0500

    Created the persist function. Accepting a callback function for the request object.

--- src/database/database.js
+++ src/database/database.js
@@ -20,4 +20,15 @@ export default class Database {
       for (let field of fields) objectStore.createIndex(field, field);
     }
   }
+
+  persist(task, success) {
+    if (typeof task === "object") {
+      const transaction = this.indexedDB.transaction([this.name], "readwrite");
+      const objectStore = transaction.objectStore(this.name);
+      const request = objectStore.add(task);
+      if (typeof success === "function") request.onsuccess = success;
+    } else {
+      throw new Error("An object was expected.");
+    }
+  }
 }
