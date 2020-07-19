ebb75e94022ac2d53040d6af8d9c3432fd8c8399
DevTony101 <tonycortesano@gmail.com>
2020-07-18 23:55:51 -0500

    Creating the constructor for the Database class. It also receives the fields in the form of a string or an array to create the schema

--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,23 @@
 export default class Database {
-  // TODO: Implement the methods of the IndexedDB API
+  constructor(name, version, fields) {
+    this.name = name;
+    this.version = version;
+    this.indexedDB = {};
+    this.database = window.indexedDB.open(name, version);
+    this.database.onsuccess = () => {
+      console.log(`Database ${name}: created successfully`);
+      this.indexedDB = this.database.result;
+    }
+
+    this.database.onupgradeneeded = event => {
+      const instance = event.target.result;
+      const objectStore = instance.createObjectStore(name, {
+        keyPath: "key",
+        autoIncrement: true,
+      });
+
+      if (typeof fields === "string") fields = fields.split(",").map(s => s.trim());
+      for (let field of fields) objectStore.createIndex(field, field);
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Database from "./database/database";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const database = new Database("DBTasks", 1, "title, description");
   const form = document.querySelector("form");
   form.addEventListener("submit", saveTask);
 
