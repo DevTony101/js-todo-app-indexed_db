c07012951b914f8a4b56766e11d758f7ef04e863
Shubhankar <vishalshubhankar@gmail.com>
2020-10-24 17:48:20 +0530

    Fix: Edit a task
    Add modal and edit facility for user to edit the saved to-do tasks.
    
    fixes: #1

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import Database from "./database/database";
 document.addEventListener("DOMContentLoaded", () => {
   const database = new Database("DBTasks", 1);
   database.init("title, description", () => showTasks());
-  const form = document.querySelector("form");
+  const form = document.querySelector("#save-task");
   const tasksContainer = document.querySelector("#task-container");
   form.addEventListener("submit", saveTask);
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = closeModal;
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      closeModal();
+    }
+  }
 
   function saveTask(event) {
     event.preventDefault();
@@ -15,6 +24,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const transaction = database.persist(task, () => form.reset());
     transaction.oncomplete = () => {
       console.log("Task added successfully!");
+      showTasks();
+    }
+  }
+
+  // function for taking in the values obtained in the form of modal and sending to database
+  function changeTask(event) {
+    event.preventDefault();
+    const key = Number(event.target.getAttribute("data-id"));
+    const title = document.querySelector("#editTitle").value;
+    const description = document.querySelector("#editDescription").value;
+    const task = {title, description, key};
+    const form = document.querySelector("#task-edit")
+    const transaction = database.saveChanges(task, () => form.reset());
+    transaction.oncomplete = () => {
+      closeModal();
+      console.log("Task edited successfully!");
       showTasks();
     }
   }
@@ -48,7 +73,23 @@ document.addEventListener("DOMContentLoaded", () => {
         // Adding it to the div message header
         message.firstChild.nextSibling.appendChild(deleteButton);
         tasksContainer.appendChild(message);
+
+        // Creating the edit task button element
+        const editButton = document.createElement("button");
+        editButton.classList.add("button");
+        editButton.innerHTML = "Edit";
+        editButton.setAttribute("aria-label","edit");
+        editButton.style.marginTop = "20px";
+        editButton.onclick = editTask;
+
+        // Adding it to the div message body
+        console.log(message.children[1]);
+        // console.log(message.children[-1]);
+        message.children[1].appendChild(editButton);
+        tasksContainer.appendChild(message);
+
         cursor.continue();
+
       } else {
         if (!tasksContainer.firstChild) {
           const text = document.createElement("p");
@@ -77,5 +118,37 @@ document.addEventListener("DOMContentLoaded", () => {
       // Optional Step 3: Console log for debugging purposes
       console.log(`Task with id ${id} deleted successfully.`);
     });
+  }
+
+  // filling up the modal with values of the respective to-do task
+  function editTask(event){
+    const header = event.target.parentElement;
+    const task = header.parentElement;
+    const id = Number(task.getAttribute("data-id"));
+    const val = database.getField(id);
+    val.onsuccess = () => {
+      const {key, title, description} = val.result;
+      var editTitle = document.getElementById("editTitle");
+      editTitle.setAttribute("value",title);
+
+      var editDescription = document.getElementById("editDescription");
+      editDescription.innerHTML = description;
+    }
+    modal.style.display = "block";
+    var saveChange = document.querySelector("#btnsave");
+    saveChange.setAttribute("data-id",id);
+    saveChange.onclick = changeTask;
+  }
+
+  // setting back modal content on closing
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    var editTitle = document.getElementById("editTitle");
+    editTitle.removeAttribute("value");
+    var editDescription = document.getElementById("editDescription");
+    editDescription.innerHTML = "";
+    var saveChange = document.querySelector("#btnsave");
+    saveChange.removeAttribute("data-id");
+    modal.style.display = "none";
   }
 });
